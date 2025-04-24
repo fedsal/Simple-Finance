c6321e6ec60104c4e20d02d4fef2ca7e3366333d
fedsal <federicosalgado2002@gmail.com>
2025-04-24 07:27:48 -0300

    remove base views

--- composeApp/src/commonMain/kotlin/org/fedsal/finance/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package org.fedsal.finance
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/org/fedsal/finance/Platform.kt
+++
@@ -1,7 +0,0 @@
-package org.fedsal.finance
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
