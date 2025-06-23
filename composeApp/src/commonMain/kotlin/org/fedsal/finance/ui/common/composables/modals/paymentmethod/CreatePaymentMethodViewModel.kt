e054b2f6cc7c46525d7680b890b601b41cd6a6ad
fedsal <federicosalgado2002@gmail.com>
2025-06-23 16:56:01 -0300

    create payment method with random color

--- composeApp/src/commonMain/kotlin/org/fedsal/finance/ui/common/composables/modals/paymentmethod/CreatePaymentMethodViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/fedsal/finance/ui/common/composables/modals/paymentmethod/CreatePaymentMethodViewModel.kt
@@ -7,6 +7,7 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import org.fedsal.finance.data.paymentmethod.PaymentMethodRepository
+import org.fedsal.finance.domain.models.AppColors
 import org.fedsal.finance.domain.models.AppIcons
 import org.fedsal.finance.domain.models.PaymentMethod
 import org.fedsal.finance.domain.models.PaymentMethodType
@@ -24,11 +25,14 @@ class CreatePaymentMethodViewModel(
 
     fun create(name: String, paymentMethodType: PaymentMethodType, iconId: AppIcons, ) = viewModelScope.launch {
         runCatching {
+            val currentColors = paymentMethodRepository.read().map { AppColors.fromHex(it.color) }
+            val unusedColor = AppColors.entries.firstOrNull { it !in currentColors }
             paymentMethodRepository.create(
                 PaymentMethod(
                     name = name,
                     type = paymentMethodType,
-                    iconId = iconId.name
+                    iconId = iconId.name,
+                    color = unusedColor?.hexString ?: ""
                 )
             )
         }.onSuccess {
